356e72a524b30e346244e5e17f37cbc2052b450d
tundeph <54335828+tundeph@users.noreply.github.com>
2022-03-06 15:22:16 +0000

    Update index.js
    No changes here as suggested, just formatting due to prettier extension on my vscode

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,11 @@
-import Detail from "../modules/property-details";
-import {Banner} from "../components/banner"
+import Detail from "../modules/property-details"
+import { Banner } from "../components/banner"
 
 export default function PropertyDetails() {
   return (
     <>
       <Banner>Property Details</Banner>
-      <Detail/>
-    </>   
-  );
+      <Detail />
+    </>
+  )
 }
